253fed44abd4682dc6a6197be6f9dcec0c834b56
Phillip Schwartz <philliptschwartz@gmail.com>
2025-08-15 12:08:10 -0400

    Frontend admin permissions in buglist comp

--- public/cmps/BugList.jsx
+++ public/cmps/BugList.jsx
@@ -1,20 +1,27 @@
 const { Link } = ReactRouterDOM
 
+import { authService } from '../services/auth.service.js'
 import { BugPreview } from './BugPreview.jsx'
 
 export function BugList({ bugs, onRemoveBug, onEditBug }) {
 
+    const loggedinUser = authService.getLoggedInUser()
+
     if (!bugs) return <div>Loading...</div>
     return <ul className="bug-list">
-        {bugs.map(bug => (
-            <li key={bug._id}>
-                <BugPreview bug={bug} />
-                <section className="actions">
-                    <button><Link to={`/bug/${bug._id}`}>Details</Link></button>
-                    <button onClick={() => onEditBug(bug)}>Edit</button>
-                    <button onClick={() => onRemoveBug(bug._id)}>x</button>
-                </section>
-            </li>
-        ))}
+        {bugs.map(bug => {
+            const isOwner = loggedinUser && (loggedinUser.isAdmin || bug.creator?._id === loggedinUser._id)
+            return (
+                <li key={bug._id}>
+                    <BugPreview bug={bug} />
+                    {isOwner && (
+                        <>
+                            <button onClick={() => onEditBug(bug)}>Edit</button>
+                            <button onClick={() => onRemoveBug(bug._id)}>Delete</button>
+                        </>
+                    )}
+                </li>
+            )
+        })}
     </ul >
 }
